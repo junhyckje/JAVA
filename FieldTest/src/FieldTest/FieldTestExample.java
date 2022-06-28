c9988387437075e28a1344038936f8615b7a53b3
junhyckje <95134704+junhyckje@users.noreply.github.com>
2022-06-28 15:36:46 +0900

    Add files via upload

---
+++ FieldTest/src/FieldTest/FieldTestExample.java
@@ -0,0 +1,20 @@
+package FieldTest;
+
+public class FieldTestExample {
+
+	public static void main(String[] args) {
+		FieldValue fiv = new FieldValue();
+		System.out.println(fiv.bytefiled);
+		System.out.println(fiv.charfiled);
+		System.out.println(fiv.shortfiled);
+		System.out.println(fiv.intfiled);
+		System.out.println(fiv.floatfiled);
+		System.out.println(fiv.longfiled);
+		System.out.println(fiv.doublefiled);
+		System.out.println(fiv.booleanfiled);
+		System.out.println(fiv.arrfiled);
+		
+		
+	}
+
+}
---
+++ FieldTest/src/FieldTest/FieldValue.java
@@ -0,0 +1,14 @@
+package FieldTest;
+
+public class FieldValue {
+	byte bytefiled;
+	char charfiled;
+	short shortfiled;
+	int intfiled;
+	float floatfiled;
+	long longfiled;
+	double doublefiled;
+	boolean booleanfiled;
+	
+	int[] arrfiled;
+}
